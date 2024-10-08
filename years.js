e2d7657fb406e215be903118a5b56f3dec90febf
Angwe <github@angwe.org>
2024-10-08 17:50:02 +0300

    try

--- years.js
+++ years.js
@@ -95,7 +95,8 @@
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
                 var render = e.object.activity.render();
-                $('.full-start-new__title', render).after(orig_title).text('xxxxxxxxxxxxxx');
+                $('.full-start-new__title', render).append(orig_title)
+                $('.full--orig-title').text('xxxxxxxxxxxxxx');
             }
         });
     }
